0f45833b15d9d8221b697c1365a654fa1ce72c62
milena-mognon <milenamognon@gmail.com>
2023-10-29 17:21:43 -0300

    chore: vitest config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
-import { ENV } from 'src/env';
+import { ENV } from '@/env';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
