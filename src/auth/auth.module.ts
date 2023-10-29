3f795f9c1a5e1f782e770ac47cf2c9bbba4ab7fd
milena-mognon <milenamognon@gmail.com>
2023-10-29 15:41:44 -0300

    feat: authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ENV } from 'src/env';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      // registerAsync é usando para conseguir injetar algum serviço necessário para configurar esse módulo
+      inject: [ConfigService], // lista de serviços que devem ser injetados quando estou registrando esse módulo
+      global: true,
+      useFactory(config: ConfigService<ENV, true>) {
+        const private_key = config.get('JWT_PRIVATE_KEY', { infer: true });
+        const public_key = config.get('JWT_PUBLIC_KEY', { infer: true });
+
+        return {
+          signOptions: { algorithm: 'RS256' },
+          privateKey: Buffer.from(private_key, 'base64'),
+          publicKey: Buffer.from(public_key, 'base64'),
+        };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AuthModule {}
