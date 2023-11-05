13911433b2511ba239f15e8c1475c258c1a7c23c
milena-mognon <milenamognon@gmail.com>
2023-11-05 16:29:51 -0300

    test: create account e2e test

--- src/controllers/create-account.controller.e2e-spec.ts
+++ src/controllers/create-account.controller.e2e-spec.ts
@@ -1,3 +1,40 @@
-test('1 puls 1', () => {
-  expect(1 + 1).toBe(2);
+import request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
+
+describe('Create account (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    await app.init();
+  });
+
+  test('[POST] /accounts', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts').send({
+      name: 'Jane Joe',
+      email: 'jane.doe@mail.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(201);
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email: 'jane.doe@mail.com',
+      },
+    });
+
+    expect(user).toBeTruthy();
+  });
 });
